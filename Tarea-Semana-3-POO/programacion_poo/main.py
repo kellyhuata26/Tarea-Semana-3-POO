5c7d78a53ef6c760900f9be220240fbd348b2c9d
kellyhuata26 <kd.tanguilah@uea.edu.ec>
2026-06-14 19:47:35 -0500

    Update main.py

---
+++ Tarea-Semana-3-POO/programacion_poo/main.py
@@ -0,0 +1,14 @@
+# Programa principal utilizando Programación Orientada a Objetos
+
+from mascota import Mascota
+
+# Creación de objetos
+mascota1 = Mascota("Firulais", "Perro", 3)
+mascota2 = Mascota("Michi", "Gato", 2)
+
+# Mostrar información
+mascota1.mostrar_informacion()
+mascota1.hacer_sonido()
+
+mascota2.mostrar_informacion()
+mascota2.hacer_sonido()
